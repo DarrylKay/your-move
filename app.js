a8f2bd9a3a6ba04483d26a3d4910bbee4853b420
DarrylKay <38496465+DarrylKay@users.noreply.github.com>
2019-01-14 20:45:32 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ $('#contactModal').on('show.bs.modal', function (event) {
 })
 
 $('#myModal').modal('handleUpdate');
+
+
+var scroll = function(e) {
+    var linkHref = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(linkHref).offset().top
+    }, 1000);
+
+    e.preventDefault();
+};
+
+$('.toTop').click(scroll);
